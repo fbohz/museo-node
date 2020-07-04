7f3c8dd3bd1d9c3759661d8db10dcea00f6d47cb
Felipe <fb.dc.col@gmail.com>
2020-07-04 17:56:33 -0500

    mongoose schema and modelel

--- server.js
+++ server.js
@@ -19,6 +19,28 @@ mongoose.connect(DB, {
   console.log('DB CONNECTED!');
 })
 
+// mongoose schema
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'tour must have name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'tour must have price']
+  }
+})
+
+// mongoose model - use uppercase on Models
+
+const Tour = mongoose.model('Tour', tourSchema)
+
 // starting server
 const port = process.env.PORT || 4000;
 
